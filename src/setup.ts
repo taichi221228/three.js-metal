f9ee2bdfeeafe43db3bfdf2b18374d4a560cac06
たいち ひ <taichi221228@icloud.com>
2025-01-01 12:54:03 +0900

    refactor: Refactor canvas element retrieval logic
    Introduce `getCanvasElement` function to encapsulate and simplify canvas selection logic.

--- src/setup.ts
+++ src/setup.ts
@@ -4,7 +4,7 @@ export function setup() {
 	const size = { width: 1252, height: 864 };
 	const renderer = new WebGLRenderer({
 		antialias: true,
-		canvas: document.getElementById("app").querySelector("canvas"),
+		canvas: getCanvasElement(),
 	});
 	renderer.setSize(size.width, size.height);
 	renderer.setClearColor(0xccc);
@@ -15,4 +15,12 @@ export function setup() {
 	camera.position.set(0, 0, 8);
 
 	return { camera, renderer, scene, size };
+}
+
+function getCanvasElement() {
+	const app = document.getElementById("app");
+	if (!app) throw Error("Application Shell is not found.");
+	const canvas = app.querySelector("canvas");
+	if (!canvas) throw Error("<canvas /> is not found.");
+	return canvas;
 }
