daa64c38dd8120a244f67cd179ca48e95cf20722
たいち ひ <taichi221228@icloud.com>
2025-01-01 13:16:31 +0900

    feat: Add initial files for Lancellotti Chapel textures
    This commit includes a README file with author and licensing details and an index.ts file exporting texture URLs for six cube map faces.

---
+++ src/lancellotti-chapel/index.ts
@@ -0,0 +1,15 @@
+import posxImageUrl from "./+x.jpg";
+import posyImageUrl from "./+y.jpg";
+import poszImageUrl from "./+z.jpg";
+import negxImageUrl from "./-x.jpg";
+import negyImageUrl from "./-y.jpg";
+import negzImageUrl from "./-z.jpg";
+
+export const textures = [
+	posxImageUrl,
+	posyImageUrl,
+	poszImageUrl,
+	negxImageUrl,
+	negyImageUrl,
+	negzImageUrl,
+];
